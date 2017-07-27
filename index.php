cd4fd0580b4ddbcd2cd5bc5f8c8552b2a69062d7
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-27 22:42:52 +0300

    Add a pager

--- index.php
+++ index.php
@@ -13,7 +13,21 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
-                <?php // COUNT NUMBER OF FILMS
+                <?php
+                    $pager=2;
+                    if (isset($_GET['page'])) {
+
+                        $page=$_GET['page'];
+                    } else {
+                        $page = '';
+                    }
+                    if ($page =='' || $page==1) {
+                        $page_1=0;
+                    } else {
+                        $page_1=($page*$pager) - $pager;
+                    }
+
+                    // COUNT NUMBER OF FILMS
                     $query2="SELECT  id FROM films";
                     $count_films=mysqli_query($connection,$query2);
                     confirmQuery($count_films);
@@ -21,22 +35,23 @@
                 ?>
 
                 <?php // SHOW ALL FILMS
-                    $query="SELECT video_title, date_format(released_date,'%Y') as 'released_date' , image FROM films  ";
+                    $query="SELECT id,video_title, date_format(released_date,'%Y') as 'released_date' , image FROM films LIMIT $page_1,$pager";
                     $show_film=mysqli_query($connection,$query);
                     confirmQuery($show_film);
+                    $count=ceil($count/$pager);
                     while ($row = mysqli_fetch_assoc($show_film))
                     {
-
+                        $film_id            =$row['id'];
                         $film_title         =$row['video_title'];
                         $film_released_date =$row['released_date'];
                         $film_image         =$row['image'];
 
                 ?>
 
-                        <h2> <a href=""><?php echo $film_title; ?></a> </h2>
-                        <h4><span class="glyphicon glyphicon-time"></span><?php echo $film_released_date; ?> </h4> <hr>
-                        <img class="img-responsive" src="images/<?php  echo $film_image; ?>" alt=""> <hr>
-                        <a class="btn btn-primary" href="">More info <span class="glyphicon glyphicon-chevron-right"></span></a><hr>
+                        <h2> <a href="post.php?film_id=<?php echo $film_id?>"><?php echo $film_title; ?></a> <?php echo "($film_released_date)"; ?> </h2>
+                         <hr>
+                        <a href="post.php?film_id=<?php echo $film_id?>"> <img class="img-responsive" src="images/<?php  echo $film_image; ?>" ></a> <hr>
+                        <a  class="btn btn-primary" href="post.php?film_id=<?php echo $film_id?>">More info <span class="glyphicon glyphicon-chevron-right"></span></a><hr>
                 <?php
                     }
                 ?>
@@ -48,6 +63,24 @@
 
         </div>
         <!-- /.row -->
+
+        <ul class="pager">
+
+            <?php
+            for ($i=1 ; $i<=$count ; $i++)
+            {
+                if ($i == $page)// ama wata bo aw pageay ka tyayayn
+                {
+                    echo "<li><a  href='index.php?page=$i'> $i </a></li>";
+                }
+                else
+                {
+                    echo "<li><a href='index.php?page=$i'>$i</a></li>";
+                }
+            }
+            ?>
+
+        </ul>
 
         <hr>
 
