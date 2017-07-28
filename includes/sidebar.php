883d40350a1048aad3b086cf55f944f16e2eb557
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-28 21:04:56 +0300

    Change the search design

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,39 +1,28 @@
-<div class="col-md-4">
+<div class="col-md-8">
 
     <!-- Blog Search Well -->
     <div class="well">
-        <h4>Search By</h4>
         <form action="../Movie/search.php" method="post"><!-- am actiona wata har yatawa naw xoy -->
-            <select class="form-control" name="searchby" id="">
-                <option value="keyword">Keyword</option>
-                <option value="genre">Genre</option>
-                <option value="year">Year</option>
-            </select><br>
+
             <div class="input-group">
-                <input name="search" type="text" class="form-control">
+                <input name="search" type="text" class="form-control" placeholder="Search for Movies or TV Shows ... ">
                 <span class="input-group-btn">
                 <button name="submit_search" class="btn btn-default" type="submit">
                     <span class="glyphicon glyphicon-search"></span>
                  </button>
                 </span>
+                <span>
+                    <select class="form-control" name="searchby" id="">
+                        <option value="">Search BY</option>
+                    <option value="keyword">Keyword</option>
+                    <option value="genre">Genre</option>
+                    <option value="year">Year</option>
+                     </select>
+                </span>
+
             </div>
         </form>
         <!-- /.input-group -->
     </div>
-
-
-    <!-- Blog Categories Well -->
-    <div class="well">
-        <h4>Blog Categories</h4>
-        <div class="row">
-            <div class="col-lg-12">
-
-            </div>
-
-            <!-- /.col-lg-6 -->
-        </div>
-        <!-- /.row -->
-    </div>
-
 
 </div>
