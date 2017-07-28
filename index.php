2b0e9baf3ddd0411fd0a2fcf4f07411d07f65d92
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-28 21:08:13 +0300

    Add tvShows in the index.php

--- index.php
+++ index.php
@@ -9,31 +9,39 @@
     <div class="container">
 
         <div class="row">
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include 'includes/sidebar.php'; ?>
+        </div>
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+        <div class="row">
 
-                <?php
-                    $pager=2;
-                    if (isset($_GET['page'])) {
+            <?php
+            $pager=2;
+            if (isset($_GET['page'])) {
 
-                        $page=$_GET['page'];
-                    } else {
-                        $page = '';
-                    }
-                    if ($page =='' || $page==1) {
-                        $page_1=0;
-                    } else {
-                        $page_1=($page*$pager) - $pager;
-                    }
+                $page=$_GET['page'];
+            } else {
+                $page = '';
+            }
+            if ($page =='' || $page==1) {
+                $page_1=0;
+            } else {
+                $page_1=($page*$pager) - $pager;
+            }
 
-                    // COUNT NUMBER OF FILMS
-                    $query2="SELECT  id FROM films";
-                    $count_films=mysqli_query($connection,$query2);
-                    confirmQuery($count_films);
-                    $count = mysqli_num_rows($count_films);
-                ?>
+            // COUNT NUMBER OF FILMS
+            $query2="SELECT  id FROM films";
+            $count_films=mysqli_query($connection,$query2);
+            confirmQuery($count_films);
+            $count = mysqli_num_rows($count_films);
+            ?>
 
+
+
+            <!-- MOVES COLUMN -->
+            <div class="col-md-5">
+                <h2>Movies</h2>
+                <hr>
                 <?php // SHOW ALL FILMS
                     $query="SELECT id,video_title, date_format(released_date,'%Y') as 'released_date' , image FROM films LIMIT $page_1,$pager";
                     $show_film=mysqli_query($connection,$query);
@@ -49,7 +57,7 @@
                 ?>
 
                         <h2> <a href="post.php?film_id=<?php echo $film_id?>"><?php echo $film_title; ?></a> <?php echo "($film_released_date)"; ?> </h2>
-                         <hr>
+
                         <a href="post.php?film_id=<?php echo $film_id?>"> <img class="img-responsive" src="images/<?php  echo $film_image; ?>" ></a> <hr>
                         <a  class="btn btn-primary" href="post.php?film_id=<?php echo $film_id?>">More info <span class="glyphicon glyphicon-chevron-right"></span></a><hr>
                 <?php
@@ -57,11 +65,36 @@
                 ?>
 
             </div>
+            <div class="col-md-1">    </div>
 
-            <!-- Blog Sidebar Widgets Column -->
-            <?php include 'includes/sidebar.php'; ?>
+
+            <!-- TV SHOWS COLUMN -->
+            <div class="col-md-5">
+                <h2>TV Shows</h2>
+                <hr>
+                <?php
+                    $sql="SELECT id,tvshow_title, image FROM tv_shows";
+                    $show_tvshow=mysqli_query($connection,$sql);
+                    confirmQuery($show_tvshow);
+                    while ($row2=mysqli_fetch_assoc($show_tvshow))
+                    {
+                        $tvshow_id=$row2['id'];
+                        $show_title=$row2['tvshow_title'];
+                        $show_image=$row2['image'];
+                ?>
+                <h2> <a href="post.php?tvshow_id=<?php echo $tvshow_id?>"><?php echo $show_title; ?></a> </h2>
+                <a href="post.php?tvshow_id=<?php echo $tvshow_id?>"> <img class="img-responsive" src="images/GOTH/<?php  echo $show_image; ?>" ></a> <hr>
+                <a  class="btn btn-primary" href="post.php?tvshow_id=<?php echo $tvshow_id?>">More info <span class="glyphicon glyphicon-chevron-right"></span></a><hr>
+                <?php
+                    }
+                ?>
+
+            </div>
+
 
         </div>
+
+
         <!-- /.row -->
 
         <ul class="pager">
