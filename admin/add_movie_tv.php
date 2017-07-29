e8c7632e6736fb879f6e40a64671852226e102c7
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-29 17:20:58 +0300

    Create add_movie_tv.php to add each one

---
+++ admin/add_movie_tv.php
@@ -0,0 +1,51 @@
+<?php include "admin_includes/admin_header.php"; ?>
+<div id="wrapper">
+        <!-- Navigation -->
+        <?php include "admin_includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Blank Page
+                            <small>Subheading</small>
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+
+                <?php
+                    if (isset($_GET['order']))
+                    {
+                        $order=$_GET['order'];
+
+                        switch ($order)
+                        {
+                            case 'addmovie':
+                                include "admin_includes/add_movie.php";
+                                break;
+                            case 'addtv':
+                                break;
+                            case 'addseason';
+                                break;
+                            case 'addepisode':
+                                break;
+                        }
+                    }
+                ?>
+
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+<?php include "admin_includes/admin_footer.php"; ?>
