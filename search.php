5b2f4aed77e9233c0ab546f048896762375f767f
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-29 11:08:32 +0300

    Search for TV Shows by keyword

--- search.php
+++ search.php
@@ -23,9 +23,10 @@
             switch ($search_by)
             {
                 case 'keyword':
-                    $query="SELECT id,video_title, date_format(released_date,'%Y') as 'released_date' , image FROM films WHERE video_title LIKE '%$search_name%'  ";
+                    $query="SELECT id, video_title, date_format(released_date,'%Y') as 'released_date' ,image FROM films WHERE video_title LIKE '%$search_name%'  ";
                     $show_film=mysqli_query($connection,$query);
                     confirmQuery($show_film);
+                    if (mysqli_num_rows($show_film)>0){
                     while ($row = mysqli_fetch_assoc($show_film))
                     {
                         $film_id            =$row['id'];
@@ -33,12 +34,33 @@
                         $film_released_date =$row['released_date'];
                         $film_image         =$row['image'];
                         ?>
-                        <h2> <a href="post.php?film_id=<?php echo $film_id?>"><?php echo $film_title; ?></a> <?php echo "($film_released_date)"; ?> </h2>
-                        <hr>
+                        <h2> <a href="post.php?film_id=<?php echo $film_id?>"><?php echo $film_title; ?></a> <?php echo "($film_released_date)"; ?> </h2> <hr>
                         <a href="post.php?film_id=<?php echo $film_id?>"> <img class="img-responsive" src="images/<?php  echo $film_image; ?>" ></a> <hr>
                         <a  class="btn btn-primary" href="post.php?film_id=<?php echo $film_id?>">More info <span class="glyphicon glyphicon-chevron-right"></span></a><hr>
+
+
                     <?php
                     }
+                    } // END IF
+
+                    else{
+                    $sql="SELECT id, tvshow_title, image, overview FROM tv_shows WHERE  tv_shows.tvshow_title LIKE '%$search_name%'";
+                    $show_tvshow=mysqli_query($connection,$sql);
+                    confirmQuery($show_tvshow);
+                    while ($row1 = mysqli_fetch_assoc($show_tvshow))
+                    {
+                        $tvshow_id          =$row1['id'];
+                        $tvshow_title       =$row1['tvshow_title'];
+                        $tvshow_image       =$row1['image'];
+                        $tvshow_overview    =$row1['overview'];
+                    ?>
+
+                        <h2> <a href="post.php?tvshow_id=<?php echo $tvshow_id?>"><?php echo $tvshow_title; ?></a> </h2><hr>
+                        <a href="post.php?tvshow_id=<?php echo $tvshow_id?>"> <img class="img-responsive" src="images/GOTH/<?php  echo $tvshow_image; ?>" ></a> <hr>
+                        <h2>Overview</h2><?php echo $tvshow_overview ?><br><br>
+                        <a  class="btn btn-primary" href="seasons.php?tvshow_id=<?php echo $tvshow_id?>">View all seasons <span class="glyphicon glyphicon-chevron-right"></span></a><hr>
+                    <?php
+                    }}
                     break;
 
 
