c5bf32e6bd0620aa06a370629659e46067d06779
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-30 23:24:20 +0300

    destroy session and go to header

--- includes/logout.php
+++ includes/logout.php
@@ -3,10 +3,9 @@
 <?php
 
 $_SESSION['username']=null; // batal krdnaway session yan cancel krdny session
-$_SESSION['firstname']=null;
-$_SESSION['lastname']=null;
+$_SESSION['user_id']=null;
 $_SESSION['user_role']=null;
 
-header("Location: ../index.php");
+header("Location: ../includes/header.php?destroy=session");
 
 ?>
