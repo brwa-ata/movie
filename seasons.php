b7003ed26cec5de7e39bb0ae6df6b961af8ab85a
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-31 01:18:36 +0300

    Fix condition

--- seasons.php
+++ seasons.php
@@ -14,33 +14,36 @@
 
         <div class="row">
             <?php
-            if ($_GET['view_season'])
+            if (isset($_GET['tvshow_id']))
             {
-                $get_tvshow_id=$_GET['view_season'];
-            }
+                 $the_tvshow_id = $_GET['tvshow_id'];
+
+
             ?>
             <!-- SEASON IMAGE -->
             <div class="col-md-8">
                 <?php
-                    $query="SELECT season_of_tvshow.id ,  season_of_tvshow.image,  season_of_tvshow.overview,  season_of_tvshow.year,  season_of_tvshow.season  
-                                from season_of_tvshow  INNER JOIN tv_shows ON season_of_tvshow.tv_shows_id=tv_shows.id  WHERE tv_shows.id=$get_tvshow_id";
-                    $get_season=mysqli_query($connection,$query);
-                    confirmQuery($get_season);
-                    while ($row=mysqli_fetch_assoc($get_season))
-                    {
-                        $season_id=$row['id'];
-                        $season_number = $row['season'];
-                        $season_overview = $row['overview'];
-                        $season_image = $row['image'];
-                        $season_year = $row['year'];
+                $query = "SELECT season_of_tvshow.id ,  season_of_tvshow.image,  season_of_tvshow.overview,  season_of_tvshow.year,  season_of_tvshow.season  
+                                from season_of_tvshow  INNER JOIN tv_shows ON season_of_tvshow.tv_shows_id=tv_shows.id  WHERE tv_shows.id=$the_tvshow_id";
+                $get_season = mysqli_query($connection, $query);
+                confirmQuery($get_season);
+                while ($row = mysqli_fetch_assoc($get_season)) {
+                    $season_id = $row['id'];
+                    $season_number = $row['season'];
+                    $season_overview = $row['overview'];
+                    $season_image = $row['image'];
+                    $season_year = $row['year'];
                     ?>
-                <h3><?php echo $season_number; ?> <?php echo '('.$season_year.')'; ?></h3>
-                        <a href="episodes.php?season_id=<?php echo $season_id; ?>"><img class="img-responsive" src="images/GOTH/<?php echo $season_image; ?> "></a>
+                    <h3><?php echo $season_number; ?><?php echo '(' . $season_year . ')'; ?></h3>
+                    <a href="episodes.php?season_id=<?php echo $season_id; ?>&tvshow_id=<?php echo $the_tvshow_id ?>"><img
+                                class="img-responsive" src="images/GOTH/<?php echo $season_image; ?> "></a>
 
-                        <h3>Overview </h3>
-                        <?php echo $season_overview; ?><hr>
-                        <?php
-                    }
+                    <h3>Overview </h3>
+                    <?php echo $season_overview; ?>
+                    <hr>
+                    <?php
+                }
+                }
                 ?>
             </div>
 
