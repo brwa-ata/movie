926ebb9e0ea2b0763b9396d000abee358eddd07f
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-30 10:30:40 +0300

    Show the data statistics

--- admin/index.php
+++ admin/index.php
@@ -1,37 +1,177 @@
 <?php include "admin_includes/admin_header.php"; ?>
+<?php include 'functions.php'; ?>
 <div id="wrapper">
         <!-- Navigation -->
         <?php include "admin_includes/admin_navigation.php"; ?>
 
-        <div id="page-wrapper">
+    <div id="page-wrapper">
 
-            <div class="container-fluid">
+        <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome to Admin
+                        <small><?php echo $_SESSION['username']; ?></small>
+                    </h1>
+
+                </div>
+            </div>
+
+            <!-- /.row -->
+
+            <!-- /.row -->
+
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-file-text fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+
+                                    <?php
+
+                                    $films_count=countRecord('films'); // pet ale ka chan post haya chwnka aw har royak 7sab aka
+                                    echo " <div class='huge'>$films_count</div>";
+
+                                    ?>
+                                    <div>Posts</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="posts.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
                     </div>
                 </div>
-                <!-- /.row -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-comments fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+
+                                    <?php
+
+                                    $tvshow_count=countRecord('tv_shows');
+                                    echo "<div class='huge'>$tvshow_count</div>";
+                                    ?>
+
+                                    <div>Comments</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="comments.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-user fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+
+                                    <?php
+                                        $user_count=countRecord('users');
+                                        echo "<div class='huge'>$user_count</div>";
+                                    ?>
+
+                                    <div> Users</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="users.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
+
+
+            <?php
+
+            /***
+             * bang krdnaway functiony checkStatus() ka recordy har yak la tablekan adozetawa ba pey marjaka
+             */
+            $film_count=countRecord('films');
+
+            $tv_shows_count=countRecord('tv_shows');
+
+            $users_count=countRecord('users');
+
+            ?>
+
+
+            <div class="row">
+
+                <script type="text/javascript">
+                    google.charts.load('current', {'packages':['bar']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                            ['Data', 'Count'],
+
+                            <?php
+
+                            $element_text=['Films','TV Shows','Users'];
+                            $element_count=[$film_count,$tv_shows_count,$users_count];
+
+                            for ($i=0 ; $i<3;$i++)
+                            {
+                                echo "['$element_text[$i]'" . "," . "$element_count[$i]],";
+                            }
+
+                            ?>
+
+                        ]);
+
+                        var options = {
+                            chart: {
+                                title: '',
+                                subtitle: '',
+                            }
+                        };
+
+                        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
+                    }
+                </script>
+
+                <!-- am scriptay xwarawa pewsyta bo kar pe krdny scriptakay sarawa -->
+                <div id="columnchart_material" style="width: auto; height: 500px;"></div>
+
+            </div>
 
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
 
     </div>
-    <!-- /#wrapper -->
+
+    <!-- /#page-wrapper -->
 <?php include "admin_includes/admin_footer.php"; ?>
