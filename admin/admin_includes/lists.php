a587fdf4a9925c3850d86c69ff06254f36906b77
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-31 17:16:49 +0300

    Getting the list id

--- admin/admin_includes/lists.php
+++ admin/admin_includes/lists.php
@@ -1,20 +1,24 @@
 <div class="col-md-6">
     <a class=" btn btn-success" href="index.php?create_list=<?php echo $_SESSION['user_id'];?>">Create new list</a>
     <?php
-        $sql="SELECT lists.listname,lists.list_image FROM lists
+        $sql="SELECT lists.id, lists.listname,lists.list_image FROM lists
               INNER JOIN users ON users.id=lists.users_id
-              INNER JOIN films ON films.id=lists.films_id
+              RIGHT JOIN films ON films.id=lists.films_id
               LEFT JOIN episode_of_seasons ON episode_of_seasons.id=lists.	episode_id
               WHERE lists.users_id={$_SESSION['user_id']}
               GROUP BY lists.listname";
         $show_lists=mysqli_query($connection,$sql);
         while ($row=mysqli_fetch_assoc($show_lists))
         {
+            $list_id=$row['id'];
             $list_name=$row['listname'];
             $list_image=$row['list_image'];
             ?>
+
             <h4><?php echo $list_name?></h4>
-            <a href="index.php?image_list=<?php echo $_SESSION['user_id'];?>"><img src="../images/<?php echo $list_image?>" alt=""></a>
+            <a href="index.php?image_list=<?php echo $_SESSION['user_id'];?>&list_name=<?php echo $list_name; ?>">
+                <img src="../images/<?php echo $list_image?>" alt=""></a>
+            <hr>
     <?php
         }
     ?>
