b1b8bbd7b664a67f3fd6c909801d7887a5f06950
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-31 08:58:13 +0300

    Seperate each table , fix the design

--- admin/admin_includes/watchlists.php
+++ admin/admin_includes/watchlists.php
@@ -1,19 +1,19 @@
-<h3>Watchlist</h3>
+<h3 class="text-center">Watchlist</h3>
+<div class="col-xs-6">
 <table class="table table-bordered table-hover">
 
     <thead>
     <thead>
     <tr>
         <th>Films</th>
-        <th>TV show Episode</th>
+
     </tr>
     </thead>
     </thead>
     <tbody>
     <?php
-    $query="SELECT films.id AS 'film_id', films.video_title, episode_of_seasons.id, episode_of_seasons.episode_name  FROM films 
+    $query="SELECT films.id AS 'film_id', films.video_title  FROM films 
                             INNER JOIN watchlist ON  films.id=watchlist.film_id
-                            LEFT JOIN episode_of_seasons ON episode_of_seasons.id= watchlist.episode_id
                             WHERE watchlist.user_id={$the_user_id}";
     $fav_table=mysqli_query($connection,$query);
     confirmQuery($fav_table);
@@ -21,14 +21,46 @@
     {
         $film_id=$row['film_id'];
         $film_title=$row['video_title'];
-        $epo_title=$row['episode_name'];
         ?>
         <tr>
             <td><a href="../post.php?film_id=<?php echo $film_id;?>"><?php echo $film_title; ?></a></td>
-            <td><?php echo $epo_title; ?></td>
         </tr>
         <?php
     }
     ?>
     </tbody>
 </table>
+</div>
+
+<div class="col-xs-6">
+    <table class="table table-bordered table-hover">
+
+        <thead>
+        <thead>
+        <tr>
+            <th>Episode</th>
+        </tr>
+        </thead>
+        </thead>
+        <tbody>
+        <?php
+        $query="SELECT episode_of_seasons.id, episode_of_seasons.episode_name  FROM episode_of_seasons 
+                                INNER JOIN watchlist ON  episode_of_seasons.id=watchlist.episode_id
+                                WHERE watchlist.user_id={$the_user_id}";
+        $fav_table=mysqli_query($connection,$query);
+        confirmQuery($fav_table);
+        while ($row=mysqli_fetch_assoc($fav_table))
+        {
+            $episode_id=$row['id'];
+            $episode_title=$row['episode_name'];
+
+            ?>
+            <tr>
+                <td><?php echo $episode_title; ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
