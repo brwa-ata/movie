2e64639ec3b3149511f7cff84d618c4a0cace557
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-07-31 09:03:06 +0300

    Seperate each table , fix the design

--- admin/admin_includes/favorite.php
+++ admin/admin_includes/favorite.php
@@ -1,34 +1,65 @@
 <h3>Favorite</h3>
-<table class="table table-bordered table-hover">
+<div class="col-xs-6">
+    <table class="table table-bordered table-hover">
 
-    <thead>
-    <thead>
-    <tr>
-        <th>Films</th>
-        <th>TV show Episode</th>
-    </tr>
-    </thead>
-    </thead>
-    <tbody>
-    <?php
-    $query="SELECT films.id AS 'film_id', films.video_title, episode_of_seasons.id, episode_of_seasons.episode_name  FROM films 
-                            INNER JOIN favorite ON  films.id=favorite.films_id
-                            LEFT JOIN episode_of_seasons ON episode_of_seasons.id= favorite.episode_id
-                            WHERE favorite.user_id={$user_id}";
-    $fav_table=mysqli_query($connection,$query);
-    confirmQuery($fav_table);
-    while ($row=mysqli_fetch_assoc($fav_table))
-    {
-        $film_id=$row['film_id'];
-        $film_title=$row['video_title'];
-        $epo_title=$row['episode_name'];
-        ?>
+        <thead>
+        <thead>
         <tr>
-            <td><a href="../post.php?film_id=<?php echo $film_id;?>"><?php echo $film_title; ?></a></td>
-            <td><?php echo $epo_title; ?></td>
+            <th>Films</th>
         </tr>
+        </thead>
+        </thead>
+        <tbody>
         <?php
-    }
-    ?>
-    </tbody>
-</table>
+        $query="SELECT films.id AS 'film_id', films.video_title  FROM films 
+                                INNER JOIN favorite ON  films.id=favorite.films_id
+                                WHERE favorite.user_id={$user_id}";
+        $fav_table=mysqli_query($connection,$query);
+        confirmQuery($fav_table);
+        while ($row=mysqli_fetch_assoc($fav_table))
+        {
+            $film_id=$row['film_id'];
+            $film_title=$row['video_title'];
+            ?>
+            <tr>
+                <td><a href="../post.php?film_id=<?php echo $film_id;?>"><?php echo $film_title; ?></a></td>
+            </tr>
+            <?php
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
+
+<div class="col-xs-6">
+    <table class="table table-bordered table-hover">
+
+        <thead>
+        <thead>
+        <tr>
+            <th>Episode</th>
+        </tr>
+        </thead>
+        </thead>
+        <tbody>
+        <?php
+        $query="SELECT episode_of_seasons.id, episode_of_seasons.episode_name  FROM episode_of_seasons 
+                                INNER JOIN favorite ON  episode_of_seasons.id=favorite.episode_id
+                                WHERE favorite.user_id={$user_id}";
+        $fav_table=mysqli_query($connection,$query);
+        confirmQuery($fav_table);
+        while ($row=mysqli_fetch_assoc($fav_table))
+        {
+            $episode_id=$row['id'];
+            $episode_title=$row['episode_name'];
+
+            ?>
+            <tr>
+                <td><?php echo $episode_title; ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
