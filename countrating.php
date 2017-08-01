61b060477d6ef98e0a2d4348e82dd1760b000d10
Brwa Ata SaedHamalw <brwa.ata.hamalaw@gmail.com>
2017-08-01 14:05:05 +0300

    Check if the user rated a film before

--- countrating.php
+++ countrating.php
@@ -7,38 +7,56 @@
         $film_id=$_GET['film_id'];
         $user_id=$_GET['user_id'];
 
-       switch ($rate)
-       {
-           case 1:
-               $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
-               $rating=mysqli_query($connection,$sql);
-               confirmQuery($rating);
-               header("Location: post.php?film_id=$film_id");
-               break;
-           case 2:
-               $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
-               $rating=mysqli_query($connection,$sql);
-               confirmQuery($rating);
-               header("Location: post.php?film_id=$film_id");
-               break;
-           case 3:
-               $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
-               $rating=mysqli_query($connection,$sql);
-               confirmQuery($rating);
-               header("Location: post.php?film_id=$film_id");
-               break;
-           case 4:
-               $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
-               $rating=mysqli_query($connection,$sql);
-               confirmQuery($rating);
-               header("Location: post.php?film_id=$film_id");
-               break;
-           case 5:
-               $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
-               $rating=mysqli_query($connection,$sql);
-               confirmQuery($rating);
-               header("Location: post.php?film_id=$film_id");
-               break;
-       }
+        $query="SELECT * FROM rating WHERE  film_id={$film_id}";
+        $ex=mysqli_query($connection,$query);
+        confirmQuery($ex);
+        while ($row=mysqli_fetch_assoc($ex))
+        {
+            $db_user_id=$row['user_id'];
+            $db_film_id=$row['film_id'];
+
+            if ($user_id != $db_user_id && $film_id!= $db_film_id)
+            {
+                switch ($rate)
+                {
+                    case 1:
+                        $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
+                        $rating=mysqli_query($connection,$sql);
+                        confirmQuery($rating);
+                        header("Location: post.php?film_id=$film_id");
+                        break;
+                    case 2:
+                        $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
+                        $rating=mysqli_query($connection,$sql);
+                        confirmQuery($rating);
+                        header("Location: post.php?film_id=$film_id");
+                        break;
+                    case 3:
+                        $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
+                        $rating=mysqli_query($connection,$sql);
+                        confirmQuery($rating);
+                        header("Location: post.php?film_id=$film_id");
+                        break;
+                    case 4:
+                        $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
+                        $rating=mysqli_query($connection,$sql);
+                        confirmQuery($rating);
+                        header("Location: post.php?film_id=$film_id");
+                        break;
+                    case 5:
+                        $sql="INSERT INTO rating (rated,film_id,user_id) VALUES ($rate,$film_id,$user_id)";
+                        $rating=mysqli_query($connection,$sql);
+                        confirmQuery($rating);
+                        header("Location: post.php?film_id=$film_id");
+                        break;
+                }
+            }
+            else
+            {
+                echo "<script> alert('You already rated this film'); </script>";
+            }
+        }
+
+
     }
 ?>
